c982729ffa676d362a759d354b2a98de24b2e1d9
KyeongHwanMin <h45652@naver.com>
2021-09-14 13:26:35 +0900

    Testing our API : 브라우져를 통해 값을 넣을 수 있다.

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -18,7 +18,14 @@ from django.urls import include, path
 from rest_framework import routers
 from tutorial.quickstart import views
 
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# 자동 URL 라우팅을 사용하여 API를 연결한다.
+# 또한 검색 가능한 API에 대한 로그인 URL을 포함한다.
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
