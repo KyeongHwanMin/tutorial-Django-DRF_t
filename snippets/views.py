bcfe412f0239164115c68bef9f8dc467f3444197
KyeongHwanMin <h45652@naver.com>
2021-09-14 17:32:07 +0900

    feat(views.py) : permissions 인증 권한 설정

--- snippets/views.py
+++ snippets/views.py
@@ -3,6 +3,7 @@ from snippets.serializers import SnippetSerializer
 from rest_framework import generics
 from django.contrib.auth.models import User
 from snippets.serializers import UserSerializer
+from rest_framework import permissions
 
 
 class UserList(generics.ListAPIView):
@@ -18,8 +19,13 @@ class UserDetail(generics.RetrieveAPIView):
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
