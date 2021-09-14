c06e715b66e3f7470b288b709120660f09e602b5
KyeongHwanMin <h45652@naver.com>
2021-09-14 21:18:48 +0900

    feat(snippets/urls.py) : ViewSet 클래스는 URL conf를 직접 디자인 할 필요 없다. Router 클래스를 사용하여 자동처리

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,9 +1,22 @@
-from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 from django.urls import path, include
 from snippets.views import SnippetViewSet, UserViewSet, api_root
 from rest_framework import renderers
+from rest_framework.routers import DefaultRouter
+
+
+# router를 생성하고 viewset을 등록한다
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+# API URL은 router에 의해 자동적으로 결정되어진다
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
+]
+
 
 snippet_list = SnippetViewSet.as_view({
     'get': 'list',
